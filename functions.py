49984b51e2482788720eb0aa78e8f3564220b778
Rohit Banerjee <rohitbanerje@gmail.com>
2021-01-13 16:21:23 -0600

    updated comments,func annotations

--- functions.py
+++ functions.py
@@ -2,6 +2,10 @@ import csv
 import numpy as np
 
 def parse(n,length):
+    """
+    Returns either latitude or longitude data up to the
+    row equal to the length parameter.
+    """
     with open('Crimes_History.csv') as file:
         csv_reader = csv.reader(file, delimiter = ",")
         result_count = 0
@@ -18,7 +22,6 @@ def parse(n,length):
                 if row[19] not in (None, ""):
                     lat.append(float(row[19]))
                     long.append(float(row[20]))
-#                    print(f'\t{row[19]},{row[20]}')
                     line_count += 1
                     result_count += 1
     if n == 19:
@@ -27,12 +30,15 @@ def parse(n,length):
         return(long)
 
 def write(n):
-	result = {}
-	for i in range(1,n+1):
-		if i%3 == 1:
-			result[i] = 'r'
-		elif i%3 == 2:
-			result[i] = 'g'
-		elif i%3 == 0:
-			result[i] = 'b'
-	return(result)
+    """
+    Assigns r/g/b sequentially to n number of nodes
+    """
+    result = {}
+    for i in range(1,n+1):
+            if i%3 == 1:
+                    result[i] = 'r'
+            elif i%3 == 2:
+                    result[i] = 'g'
+            elif i%3 == 0:
+                    result[i] = 'b'
+    return(result)
--- kcluster.py
+++ kcluster.py
@@ -1,66 +1,73 @@
-import pandas as pd
-import numpy as np
+#Rohit Banerjee, 2019
+
+import pandas as pd #df
+import numpy as np  #mean, sqrt, random
 import matplotlib.pyplot as plt
-import copy
-import functions
-
-latitudes = functions.parse(19,15000)
-longitudes = functions.parse(20,15000)
-df = pd.DataFrame({
-    'x':longitudes,
-    'y':latitudes
-})
-
-np.random.seed(200)
-k = 15
-centroids = {
-    i+1: [-87.95+np.random.randint(0,4500)*(1/10000), 41.63+np.random.randint(0,4000)*(1/10000)]
-    for i in range(k)
-}
-colmap = functions.write(k)
+from time import time #for random seed
+import copy #deepcopy
+import functions #parse, write
 
 def assignment(df, centroids):
+    """
+    Assign each data point a closest centroid
+    """
     for i in centroids.keys():
-        # sqrt((x1 - x2)^2 - (y1 - y2)^2)
         df['distance_from_{}'.format(i)] = (
-            np.sqrt(
-                (df['x'] - centroids[i][0]) ** 2
-                + (df['y'] - centroids[i][1]) ** 2
-            )
-        )
+            np.sqrt((df['x'] - centroids[i][0]) ** 2 + (df['y'] - centroids[i][1]) ** 2))
     centroid_distance_cols = ['distance_from_{}'.format(i) for i in centroids.keys()]
-    df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
-    df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
-#    df['color'] = df['closest'].map(lambda x: colmap[x])
-    return df
+    df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)            #finds minimum distance from any given data point to all centroids
+    df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))#replaces "closest" column with identifier of closest centroid
+    return(df)
 
-df = assignment(df, centroids)
-
-old_centroids = copy.deepcopy(centroids)
-
-def update(k):
+def update(df,centroids):
+    """
+    Sets centroids to the mean location of their closest data points.
+    Note that some centroids are set to NaN in this func as centroids with
+    no "closest" data points are nullified and ignored from then on out.
+    This is due to the dataset not being uniformly spread throughout the region
+    like the centroids. This results in usually <15 of the original 15 centroids
+    surviving.  
+    """
     for i in centroids.keys():
         centroids[i][0] = np.mean(df[df['closest'] == i]['x'])
         centroids[i][1] = np.mean(df[df['closest'] == i]['y'])
-    return k
+    return(centroids)
 
-centroids = update(centroids)
-df = assignment(df, centroids)
-
-while True:
-    closest_centroids = df['closest'].copy(deep=True)
-    centroids = update(centroids)
+def main():
+    #initializing data frame and centroids
+    latitudes = functions.parse(19,15000)
+    longitudes = functions.parse(20,15000)
+    df = pd.DataFrame({
+        'x':longitudes,
+        'y':latitudes
+    })
+    k = 15
+    np.random.seed(int(time()))
+    centroids = {
+        #this range is specific to the chicago dataset
+        i+1: [-87.95+np.random.randint(0,4500)*(1/10000), 41.63+np.random.randint(0,4000)*(1/10000)]
+        for i in range(k)
+    }
+    #k-means clustering
+    colmap = functions.write(k)
     df = assignment(df, centroids)
-    if closest_centroids.equals(df['closest']):
-        break
+    while True:
+        closest_centroids = df['closest'].copy(deep=True)
+        centroids = update(df,centroids)
+        df = assignment(df, centroids)
+        if closest_centroids.equals(df['closest']):
+            break
+    #plotting
+    fig = plt.figure(figsize=(7, 7))
+    plt.scatter(df['x'], df['y'], alpha=0.5, edgecolor='k')
+    for i in centroids.keys():
+        plt.scatter(*centroids[i], color="r")
+    plt.xlim(-87.95, -87.5)
+    plt.ylim(41.63, 42.03)
+    print({i:centroids[i] for i in centroids if pd.isnull(centroids[i][0]) != True}) #Prints all non-NaN centroids
+    plt.show()
+    return(1)
 
-fig = plt.figure(figsize=(7, 7))
-#plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
-plt.scatter(df['x'], df['y'], alpha=0.5, edgecolor='k')
-for i in centroids.keys():
-#    plt.scatter(*centroids[i], color=colmap[i])
-    plt.scatter(*centroids[i], color="r")
-plt.xlim(-87.95, -87.5)
-plt.ylim(41.63, 42.03)
-plt.show()
-print(centroids)
+#driver code
+if __name__ == '__main__':
+    print(main())
